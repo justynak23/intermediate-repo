bf5adcb5d129ff26cdf05ffd98cdf5f54a10782c
justynak23 <justynak@spacelift.io>
2025-06-04 16:07:27 +0100

    Update resources.tf

--- intermediate-repo-main/Getting-started/resources.tf
+++ intermediate-repo-main/Getting-started/resources.tf
@@ -18,7 +18,7 @@ resource "spacelift_stack" "managed" {
   administrative = true
   repository   = "intermediate-repo"
   branch       = "main"
-  project_root = "Drift-Detection-Stack"
+  project_root = "intermediate-repo-main/Drift-Detection-Stack"
   space_id    = spacelift_space.intermediate-repo.id
   depends_on  = [data.spacelift_current_stack.this]
 }
@@ -36,7 +36,7 @@ resource "spacelift_stack" "Stack-Dependencies" {
   description = "Your first stack managed by Terraform"
   repository   = "intermediate-repo"
   branch       = "main"
-  project_root = "Stack-Dependencies"
+  project_root = "intermediate-repo-main/Stack-Dependencies"
   space_id    = spacelift_space.intermediate-repo.id
   depends_on = [spacelift_space.intermediate-repo]
 }
@@ -50,7 +50,7 @@ resource "spacelift_stack" "private_worker" {
   description = "A stack to create your private_worker"
   space_id = spacelift_space.intermediate-repo.id
   administrative    = true
-  repository   = "intermediate-repo"
+  repository   = "intermediate-repo-main/intermediate-repo"
   branch       = "main"
   project_root = "Private-worker"
   depends_on = [spacelift_space.intermediate-repo]
